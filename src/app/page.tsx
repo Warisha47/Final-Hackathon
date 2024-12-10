e9958b0e7d408d640f0304d1916f61c1226c69c9
Warisha naz <warishanaz265@gmail.com>
2024-12-11 05:37:53 +0800

    changes in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 
-import TopHeader, { Header } from "../Components/TopHeader";
-import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
+
 import Hero from "../Components/Hero";
 import { HeroLogo } from "../Components/Hero";
 import FeaturedProducts, { Categories, SubCategory } from "../Components/FeaturedProducts";
 import OurProducts from "../Components/OurProducts";
-import Products from "../Components/OurProducts";
+
 
 export default function Home() {
   return (
