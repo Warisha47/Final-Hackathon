98811bfa8e6ca3e30b5ee69d6e852893c732407b
Warisha naz <warishanaz265@gmail.com>
2024-12-19 23:26:42 +0500

    changes in faqs

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 const Faqs = () => {
   return(
  <div className='md:w-[1980px]  md:h-[800px] md:mt-10 mt-12   wrapper'>
-  <h1 className='flex  md:justify-center ml-8 md:ml-0 items-center md:p-2 p-4 font-bold text-[24px]'>  Questions Looks Here
+  <h1 className='flex  md:justify-center  items-center md:p-2 p-4 font-bold text-[24px]'>  Questions Looks Here
   </h1>
   
-    <p className='h-[24px] md:w-[770px] w-[300px] md:ml-56 ml-12 font-normal text-[16px] flex items-center justify-center mt-3 '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
+    <p className='h-[24px] md:w-[770px] w-[300px] md:ml-56 font-normal text-[16px] flex items-center justify-center mt-3 '>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the </p>
   
   {/* text div */}
-  <div className='grid md:grid-cols-2 md:mx-20 mx-10 md:ml-28'>
+  <div className='grid md:grid-cols-2 md:mx-20 md:ml-28'>
   <div className='md:w-[520px] w-[300px] md:h-[150px] h-[240px] md:mt-[36px] mt-16 grid grid-cols-2 bg-[#F7F7F7] rounded-md'>
      <p className='mt-[10px] ml-[18px] md:w-[303px]  font-bold text-[18px]'>
      What types of chairs do you offer? </p>
